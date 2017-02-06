5ceac1d7dd9f4f6630128f87ba9d2e845db45518
hreclove <hreclove@gmail.com>
2017-02-06 17:16:55 +0900

    asfdfs

--- extension/sprk.js
+++ extension/sprk.js
@@ -263,21 +263,21 @@
             // 'R' 	Asynchronous reporter
             // 'h' 	Hat block (synchronous, returns boolean, true = run stack)
             en: [
-            [' ', 'Roll with angle %n, speed %n', 'roll', '0', '50'],
-            [' ', 'Roll %m.direction speed %n', 'rollDir', 'forward'],
-            [' ', 'Roll Stop','rollStop'],
-            [' ', 'LED %m.lightColor', 'light', 'red'],
-            [' ', 'LED Red:%n Green:%n Blue:%n', 'lightRGB', '255', '0', '0'],
-            ['h', 'when Collision detected', 'whenSensorDetected']
+	            [' ', 'Roll with %n degrees, speed %n', 'roll', '0', '50'],
+	            [' ', 'Roll to %m.direction , speed %n', 'rollDir', 'forward'],
+	            [' ', 'Roll Stop','rollStop'],
+	            [' ', 'set LED to %m.lightColor', 'light', 'red'],
+	            [' ', 'set LED with Red:%n Green:%n Blue:%n', 'lightRGB', '255', '0', '0'],
+	            ['h', 'when Collision detected', 'whenSensorDetected']
             ],
             ko: [
-            [' ', '이동 %n도 방향, 속도 %n', 'roll', '0', '50'],
-            [' ', '이동 %m.direction 속도 %n', 'rollDir', 'forward'],
-            [' ', '정지','rollStop'],
-            [' ', '램프색 변경, %m.lightColor', 'light', 'red'],
-            [' ', '램프색 조합, 빨강:%n 초록:%n 파랑:%n', 'lightRGB', '255', '0', '0'],
-            ['h', '충돌하면', 'whenSensorDetected']
-            ],
+	            [' ', '이동 %n도 방향, 속도 %n', 'roll', '0', '50'],
+	            [' ', '이동 %m.direction 속도 %n', 'rollDir', 'forward'],
+	            [' ', '정지','rollStop'],
+	            [' ', '램프색 바꾸기, %m.lightColor', 'light', 'red'],
+	            [' ', '램프색 조합하기, 빨강:%n 초록:%n 파랑:%n', 'lightRGB', '255', '0', '0'],
+	            ['h', '충돌하면', 'whenSensorDetected']
+            ]
         ],
         menus: {
         	en: {
